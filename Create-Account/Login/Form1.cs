b2266adbc478ace24af5e03de7c2a7855bccafe5
Sakshi Hargude <96101377+Sakshi-Hargude@users.noreply.github.com>
2024-06-13 12:47:07 +0530

    Add files via upload

---
+++ Create-Account/Login/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+        MySqlCommand command;
+        MySqlDataReader mdr;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string selectQuery = "SELECT * FROM loginform.userinfo WHERE Username = '" + txtUsername.Text + "' AND Password = '" + txtPassword.Text + "';";
+            command = new MySqlCommand(selectQuery, connection);
+            mdr = command.ExecuteReader();
+            if (mdr.Read())
+            {
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=";
+                string Query = "update loginform.userinfo set LastLogin='" + dateTimePicker1.Value + "' where Username='" + this.txtUsername.Text + "';";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();
+
+                MessageBox.Show("Login Successful!");
+                this.Hide();
+                LoginSuccessForm frm2 = new LoginSuccessForm();
+                frm2.ShowDialog();
+
+            }
+            else
+            {
+
+                MessageBox.Show("Incorrect Login Information! Try again.");
+            }
+
+            connection.Close();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string selectQuery = "SELECT * FROM loginform.userinfo WHERE Username = '" + txtUsername.Text + "';";
+            command = new MySqlCommand(selectQuery, connection);
+            mdr = command.ExecuteReader();
+            if (mdr.Read())
+            {
+                MessageBox.Show("Username not available!");
+
+            }
+            else
+            {
+
+                string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=loginform;";
+                string iquery = "INSERT INTO userinfo(`ID`,`Username`, `Password`, `DateCreated`,`LastLogin`) VALUES (NULL, '" + txtUsername.Text + "', '" + txtPassword.Text + "', '" + dateTimePicker1.Value + "', '" + dateTimePicker1.Value + "')";
+
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(iquery, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+
+                try
+                {
+                    databaseConnection.Open();
+                    MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                    databaseConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Show any error message.
+                    MessageBox.Show(ex.Message);
+                }
+
+                MessageBox.Show("Account Successfully Created!");
+            }
+
+            connection.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
---
+++ Create-Account/Login/LoginSuccessForm1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Login
+{
+    partial class LoginSuccessForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.White;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(145, 129);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(97, 23);
+            this.btnLogout.TabIndex = 0;
+            this.btnLogout.Text = "LOG OUT";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // LoginSuccessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(418, 450);
+            this.Controls.Add(this.btnLogout);
+            this.Name = "LoginSuccessForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginSuccessForm";
+            this.Load += new System.EventHandler(this.LoginSuccessForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
